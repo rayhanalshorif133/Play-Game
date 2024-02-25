c87e011554f59e0cbb6067f0e442e2e2c83c677f
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-02-25 15:15:14 +0600

    login done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check()){
+        return redirect()->route('home');
+    }else{
+        // return view('welcome');
+        return redirect()->route('login');
+    }
 });
 
 Auth::routes();
