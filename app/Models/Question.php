c27ea518bacbff8cfe8d1d0923c25a672e474818
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-02-25 15:10:56 +0600

    user create done with seeder

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'created_by',
+        'title',
+        'description',
+        'image',
+        'option_a',
+        'option_b',
+        'option_c',
+        'option_d',
+        'correct_option',
+        'score',
+        'status',
+    ];
 }
--- database/migrations/2024_02_22_063129_create_questions_table.php
+++ database/migrations/2024_02_22_063129_create_questions_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Database\Seeders\DatabaseSeeder;
+
 
 return new class extends Migration
 {
@@ -13,8 +15,22 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('created_by')->unsigned();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('option_a');
+            $table->string('option_b');
+            $table->string('option_c')->nullable();
+            $table->string('option_d')->nullable();
+            $table->string('correct_option')->enum('option_a','option_b','option_c','option_d');
+            $table->integer('score')->default(0);
+            $table->string('status')->enum('active','inactive')->default('active');
             $table->timestamps();
         });
+
+        $dbSeeder = new DatabaseSeeder();
+        $dbSeeder->run();
     }
 
     /**
